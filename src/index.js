76748bd4191d5043cdbc8d2eb48d545e3e87e775
Jioyzen <60812231+Jioyzen@users.noreply.github.com>
2024-06-26 23:58:40 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.crob.top": dockerHub,
+  "quay.crob.top": "https://quay.io",
+  "gcr.crob.top": "https://gcr.io",
+  "k8s-gcr.crob.top": "https://k8s.gcr.io",
+  "k8s.crob.top": "https://registry.k8s.io",
+  "ghcr.crob.top": "https://ghcr.io",
+  "cloudsmith.crob.top": "https://docker.cloudsmith.io",
+  "ecr.crob.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.crob.top": dockerHub,
 };
 
 function routeByHosts(host) {
